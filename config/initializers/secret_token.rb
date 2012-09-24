640732397d49e82861017790558f63b56d6e9653
Judyta <judyta.kurdys@gmail.com>
2012-09-24 17:18:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WomanNeverGiveUp::Application.config.secret_token = '91344b6245c2c4c707f40679a99ea4f6260a5f52d905c6acb3a146fe0178f79b67ae8ba532077dd43b365c0ba14e04ed22be622fad3cc0ebc6bbd722ecd6cb16'
