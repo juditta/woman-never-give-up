1a620a333ec5e4cb33191ac12e28562f3f2de016
Judyta <judyta.kurdys@gmail.com>
2012-11-11 12:10:10 +0100

    make a basic User model (including secure passwords)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
- 
   def new
-    
-    end
-
-
   end
+end
---
+++ db/migrate/20121106183703_drop_table_users.rb
@@ -0,0 +1,7 @@
+class DropTableUsers < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
