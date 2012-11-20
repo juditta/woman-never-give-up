6aa2cd9dc9bbb2c1a8281b7e77ffb68f78be2a70
Judyta <judyta.kurdys@gmail.com>
2012-11-20 19:38:57 +0100

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { self.email.downcase! }
+
+  before_save :correct 
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,4 +12,13 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private 
+    def correct 
+      self.email.downcase!
+      self.name.capitalize! 
+    end
+
+
+
 end
